95d313ad797b2aee301c1d41dec326c03224b71b
brmntyprytm <bramantyopriyou@gmail.com>
2023-02-25 21:36:48 +0700

    Second Version

--- src/Assignment1.java
+++ src/Assignment1.java
@@ -1,5 +1,17 @@
+// NAME  : Bramantyo Priyo Utomo
+// NPM   : 2206821563
+// TA    : SA
+// CLASS : KKI
+// FILE  : Assignment1.java
+//
+// START OF THE PROGRAM!!!
+
+import javax.swing.JOptionPane;
 public class Assignment1 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String in = JOptionPane.showInputDialog(null,
+                "Enter a credit card number:\n", "Credit Card Validity " +
+                "Checker", JOptionPane.PLAIN_MESSAGE);
+        long num = Long.parseLong(in);
     }
 }
