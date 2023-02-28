00341d208c42d4aeb41022b88f4748c6afec5d83
brmntyprytm <bramantyopriyou@gmail.com>
2023-02-28 09:28:53 +0700

    Second Version

--- src/Assignment1.java
+++ src/Assignment1.java
@@ -9,9 +9,41 @@
 import javax.swing.JOptionPane;
 public class Assignment1 {
     public static void main(String[] args) {
-        String in = JOptionPane.showInputDialog(null,
-                "Enter a credit card number:\n", "Credit Card Validity " +
-                "Checker", JOptionPane.PLAIN_MESSAGE);
+        String in;
         long num = Long.parseLong(in);
+        boolean quit = false;
+        do {
+            in = JOptionPane.showInputDialog(null,
+                 "Enter a Credit Card / Debit Card Number " +
+                 "as a long integer, QUIT to end:\n" + "Credit Card Validity "
+                 + "Checker", JOptionPane.PLAIN_MESSAGE);
+            if (in == null) {
+                quit = true;
+            }
+            if (in.equalsIgnoreCase("quit")) {
+                quit = true;
+            } else {
+                try {
+                    if (isValid(num) == true) {
+                        JOptionPane.showMessageDialog(null,
+                        "The number " + num + " is valid." +
+                        "Credit Card Validity " + "Checker" +
+                        JOptionPane.PLAIN_MESSAGE);
+                    } else {
+                        JOptionPane.showMessageDialog(null,
+                        "The number " + num + " is invalid." +
+                        "Credit Card Validity " + "Checker" +
+                        JOptionPane.PLAIN_MESSAGE);
+                    }
+                } catch (NumberFormatException e) {
+                    JOptionPane.showMessageDialog(null,
+                    "The " + num + " input is not allowed." +
+                    "Credit Card Validity " + "Checker" +
+                    JOptionPane.PLAIN_MESSAGE);
+                }
+            }
+        } while (!quit);
+    public static boolean isValid(long number) {
+        }
     }
 }
